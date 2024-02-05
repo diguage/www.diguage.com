68fec15580dde4b6f9bc78399eb49dc6c4d3ca27
diguage <leejun119@gmail.com>
2024-02-05 05:06:38 +0000

    Deploying to gh-pages from @ diguage/www.diguage.com@191efbd2b22b5b81a0bf602992ef709325919737 🚀

---
+++ diagrams/build.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# get base dir
+baseDir=$(cd $(dirname $BASH_SOURCE) && pwd)
+echo "baseDir=${baseDir}"
+
+params="-tsvg -SdefaultFontSize=18 -StitleFontSize=36 -SfooterFontSize=20 -StitleFontName='Source Han Sans SC' -SnoteFontName='Source Han Serif SC' -SfooterFontName='Source Han Serif SC' "
+echo ${params}
+
+echo "start to convert ${baseDir}/Hessian2Output-writeObject.diag"
+seqdiag -Tsvg ${baseDir}/Hessian2Output-writeObject.diag
+cp -f ${baseDir}/Hessian2Output-writeObject.svg ${baseDir}/../images/marshal/
+
+echo "start to convert ${baseDir}/Hessian2Input-readObject.diag"
+seqdiag -Tsvg ${baseDir}/Hessian2Input-readObject.diag
+cp -f ${baseDir}/Hessian2Input-readObject.svg ${baseDir}/../images/marshal/
+
+echo "start to convert ${baseDir}/hessian-AbstractSerializerFactory.puml"
+plantuml ${params} ${baseDir}/hessian-AbstractSerializerFactory.puml
+cp -f ${baseDir}/hessian-AbstractSerializerFactory.svg ${baseDir}/../images/marshal/
+
+echo "start to convert ${baseDir}/hessian-AbstractHessianOutput.puml"
+plantuml ${params} ${baseDir}/hessian-AbstractHessianOutput.puml
+cp -f ${baseDir}/hessian-AbstractHessianOutput.svg ${baseDir}/../images/marshal/
+
+echo "start to convert ${baseDir}/hessian-AbstractHessianInput.puml"
+plantuml ${params} ${baseDir}/hessian-AbstractHessianInput.puml
+cp -f ${baseDir}/hessian-AbstractHessianInput.svg ${baseDir}/../images/marshal/
+
+echo "start to convert ${baseDir}/hessian-Serializer.puml"
+plantuml ${params} ${baseDir}/hessian-Serializer.puml
+cp -f ${baseDir}/hessian-Serializer.svg ${baseDir}/../images/marshal/
+
+echo "start to convert ${baseDir}/hessian-Deserializer.puml"
+plantuml ${params} ${baseDir}/hessian-Deserializer.puml
+cp -f ${baseDir}/hessian-Deserializer.svg ${baseDir}/../images/marshal/
+
+echo "start to convert ${baseDir}/hessian-architecture.puml"
+plantuml ${params} ${baseDir}/hessian-architecture.puml
+cp -f ${baseDir}/hessian-architecture.svg ${baseDir}/../images/marshal/
+
+echo "start to convert ${baseDir}/hessian-bytecode.puml"
+plantuml ${params} ${baseDir}/hessian-bytecode.puml
+cp -f ${baseDir}/hessian-bytecode.svg ${baseDir}/../images/marshal/
